9851ff647a712bfcc486e66bc5c32d881e74ddf9
daizdag <115800558+daizdag@users.noreply.github.com>
2023-01-19 22:30:25 +0900

    エラーコード及びテストコードの実装及

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
+
+
   validates :nickname,           presence: true
-  validates :email,              presence: true
-  validates :encrypted_password, presence: true
-  validates :first_name,         presence: true
-  validates :last_name,          presence: true
-  validates :first_name_kana,    presence: true
-  validates :last_name_kana,     presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: 'is invalid. Include both letters and numbers'}
+
   validates :birthday,           presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end  
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
     
 end
