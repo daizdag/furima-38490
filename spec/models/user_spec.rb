7d1e428775d475b06cf332f7488eed0a50726216
daizdag <115800558+daizdag@users.noreply.github.com>
2023-01-20 14:55:08 +0900

    レビューの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,8 +43,18 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it 'passwordは半角英数字を使用しないと登録できない' do
+      it 'passwordは英字のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+      end      
+      it 'passwordは数字のみでは登録できない' do
         @user.password = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+      end
+      it 'passwordは全角では登録できない' do
+        @user.password = 'ａａａａａａ'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
       end
