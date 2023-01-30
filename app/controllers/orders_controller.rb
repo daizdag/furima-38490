6956f76cde392b43728628dc4849f4ea21d35673
daizdag <115800558+daizdag@users.noreply.github.com>
2023-01-30 18:44:34 +0900

    PAY.JPを用いたクレジットカードでの決済機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,28 @@
 class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
 end
