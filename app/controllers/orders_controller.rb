fdb11e0c8b46b082a51ce2f77d10e298367547f2
daizdag <115800558+daizdag@users.noreply.github.com>
2023-02-08 18:48:49 +0900

    テストコード修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :set_index, only: [:new, :create]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:new, :create]
   before_action :order_confirmation, only: [:new, :create]
 
   def index
@@ -34,6 +35,11 @@ class OrdersController < ApplicationController
 
   def order_confirmation
     redirect_to root_path unless @item.order.nil?
+  end
+
+  def contributor_confirmation
+    current_user == @item.user
+    redirect_to root_path 
   end
 
   def pay_item
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe OrderAddress, type: :model do
   describe '購入情報の保存' do
     before do
       user = FactoryBot.create(:user)
-      item = FactoryBot.build(:item)
-      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id)
     end
 
     context '内容に問題ない場合' do
